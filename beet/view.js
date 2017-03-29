e7d0d541aeb566dc06a75f2be85091ce07f14a8d
ei1333 <missnakakun@gmail.com>
2017-03-29 13:04:16 +0900

    beet

--- beet/view.js
+++ beet/view.js
@@ -1,9 +1,41 @@
 $(function()
 {
+  var url = "http://codeforces.com/api/user.rating?handle=ei1333";
+  var xpath = '/html/body/pre';
+  var query = "select * from json where url = '" + url + "'";
+  var yql   = "https://query.yahooapis.com/v1/public/yql?format=json&q=" + encodeURIComponent(query);
+
   $.ajax(
   {
     type     : 'GET',
-    url      : '//codeforces.com/api/user.rating?handle=ei1333',
+    url      : yql,
+    dataType : 'json',
+    timeout  : 10000,
+    cache    : false,
+    success  : function(data)
+               {
+                  console.log(data);
+               },
+  });
+/*
+  $.ajax(
+  {
+    type     : 'GET',
+    url      : 'https://codeforces.com/api/user.rating?handle=ei1333',
+    dataType : 'json',
+    timeout  : 10000,
+    cache    : false,
+    success  : function(data)
+               {
+                  console.log(data);
+               },
+  });
+*/
+
+  $.ajax(
+  {
+    type     : 'GET',
+    url      : "https://api.topcoder.com/v2/users/ei1333/statistics/data/srm",
     dataType : 'json',
     timeout  : 10000,
     cache    : false,
@@ -13,24 +45,11 @@ $(function()
                },
   });
 
-  $.ajax(
-  {
-    type     : 'GET',
-    url      : "//api.topcoder.com/v2/users/ei1333/statistics/data/srm",
-    dataType : 'json',
-    timeout  : 10000,
-    cache    : false,
-    success  : function(data)
-               {
-                  console.log(data);
-               },
-  });
 
-
-  var url = "//atcoder.jp/user/ei1333";
+  var url = "https://atcoder.jp/user/ei1333";
   var xpath = '//*[@id="main-div"]/div/div/div[2]/script[2]';
   var query = "select * from html where url = '" + url + "' and xpath = '" + xpath + "'";
-  var yql   = "//query.yahooapis.com/v1/public/yql?format=json&q=" + encodeURIComponent(query);
+  var yql   = "https://query.yahooapis.com/v1/public/yql?format=json&q=" + encodeURIComponent(query);
 
   $.ajax(
   {
