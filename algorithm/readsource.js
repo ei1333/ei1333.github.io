8385ada920f46b15cf158faac141c2b288d46967
ei1333 <missnakakun@gmail.com>
2017-06-08 11:39:29 +0900

    addsource

--- algorithm/readsource.js
+++ algorithm/readsource.js
@@ -3,7 +3,7 @@ $(function()
   $('pre code').each(function(i, block)
   {
     if(typeof $(this).attr('src') !== "undefined") {
-     $(this).load("source_code/ " + $(this).attr('src'));
+     $(this).load($(this).attr('src'));
     }
   });
   hljs.initHighlightingOnLoad();  
