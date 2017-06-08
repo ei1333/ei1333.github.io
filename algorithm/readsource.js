9b151fb5ae67c8a9e029b8ff6f0a09efc2f8fb78
ei1333 <missnakakun@gmail.com>
2017-06-08 11:36:09 +0900

    addsource

--- algorithm/readsource.js
+++ algorithm/readsource.js
@@ -3,7 +3,7 @@ $(function()
   $('pre code').each(function(i, block)
   {
     if(typeof $(this).attr('src') !== "undefined") {
-     $(this).load($(this).attr('src'));
+     $(this).load("source_code/ " + $(this).attr('src'));
     }
   });
   hljs.initHighlightingOnLoad();  
--- algorithm/source_code/segment-tree-rangeadd.cpp
+++ algorithm/source_code/segment-tree-rangeadd.cpp
@@ -1,19 +1,22 @@
+
 template< typename T >
 struct SegmentTree
 {
   const function< T(T, T) > merge;
   const T INF;
 
-  vector< T > seg;
+  vector< T > seg, add;
   int sz;
 
+
   SegmentTree(int n, bool ismax = false)
-      : INF(ismax ? numeric_limits< T >::min() : numeric_limits< T >::max()),
+      : INF(ismax ? numeric_limits< T >::min() / 5 : numeric_limits< T >::max() / 5),
         merge(ismax ? [&](T a, T b) { return (max(a, b)); } : [&](T a, T b) { return (min(a, b)); })
   {
     sz = 1;
     while(sz < n) sz <<= 1;
-    seg.assign(2 * sz - 1, INF);
+    seg.assign(2 * sz - 1, 0);
+    add.assign(2 * sz - 1, 0);
   }
 
   void set(int k, int x)
@@ -31,9 +34,9 @@ struct SegmentTree
   T rmq(int a, int b, int k, int l, int r)
   {
     if(a >= r || b <= l) return (INF);
-    if(a <= l && r <= b) return (seg[k]);
+    if(a <= l && r <= b) return (seg[k] + add[k]);
     return (merge(rmq(a, b, 2 * k + 1, l, (l + r) >> 1),
-                  rmq(a, b, 2 * k + 2, (l + r) >> 1, r)));
+                  rmq(a, b, 2 * k + 2, (l + r) >> 1, r)) + add[k]);
   }
 
   T rmq(int a, int b)
@@ -41,13 +44,15 @@ struct SegmentTree
     return (rmq(a, b, 0, 0, sz));
   }
 
-  void update(int k, T x)
+  void rangeadd(int a, int b, int x, int k, int l, int r)
   {
-    k += sz - 1;
-    seg[k] = x;
-    while(k > 0) {
-      k = (k - 1) >> 1;
-      seg[k] = merge(seg[2 * k + 1], seg[2 * k + 2]);
+    if(a >= r || b <= l) return;
+    if(a <= l && r <= b) {
+      add[k] += x;
+      return;
     }
+    rangeadd(a, b, x, 2 * k + 1, l, (l + r) >> 1);
+    rangeadd(a, b, x, 2 * k + 2, (l + r) >> 1, r);
+    seg[k] = merge(seg[2 * k + 1] + add[2 * k + 1], seg[2 * k + 2] + add[2 * k + 2]);
   }
 };
