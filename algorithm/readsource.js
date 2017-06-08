09a7a3aa7abb3338dc9a3bcd9847cfd990abefee
ei1333 <missnakakun@gmail.com>
2017-06-08 11:57:06 +0900

    escape html

--- algorithm/readsource.js
+++ algorithm/readsource.js
@@ -1,31 +1,3 @@
-var str = '& < > ` " ' + "'";
-var escapeHtml = (function (String) {
-  var escapeMap = {
-    '&': '&amp;',
-    "'": '&#x27;',
-    '`': '&#x60;',
-    '"': '&quot;',
-    '<': '&lt;',
-    '>': '&gt;'
-  };
-  var escapeReg = '[';
-  var reg;
-  for (var p in escapeMap) {
-    if (escapeMap.hasOwnProperty(p)) {
-      escapeReg += p;
-    }
-  }
-  escapeReg += ']';
-  reg = new RegExp(escapeReg, 'g');
- 
-  return function escapeHtml (str) {
-    str = (str === null || str === undefined) ? '' : '' + str;
-    return str.replace(reg, function (match) {
-      return escapeMap[match];
-    });
-  };
-}(String));
-
 $(function()
 {
   $('pre code').each(function(i, block)
@@ -33,6 +5,7 @@ $(function()
     if(typeof $(this).attr('src') !== "undefined") {
       $.get($(this).attr('src'), function(data)
       {
+console.log(data);
         $(this).text(data);
       });
     }
