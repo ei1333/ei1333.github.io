0e39e1ef9fce9ab26ba68711deed0b410b56070e
ei1333 <missnakakun@gmail.com>
2017-06-08 11:49:32 +0900

    escape html

--- algorithm/readsource.js
+++ algorithm/readsource.js
@@ -1,9 +1,37 @@
+var str = '& < > ` " ' + "'";
+var escapeHtml = (function (String) {
+  var escapeMap = {
+    '&': '&amp;',
+    "'": '&#x27;',
+    '`': '&#x60;',
+    '"': '&quot;',
+    '<': '&lt;',
+    '>': '&gt;'
+  };
+  var escapeReg = '[';
+  var reg;
+  for (var p in escapeMap) {
+    if (escapeMap.hasOwnProperty(p)) {
+      escapeReg += p;
+    }
+  }
+  escapeReg += ']';
+  reg = new RegExp(escapeReg, 'g');
+ 
+  return function escapeHtml (str) {
+    str = (str === null || str === undefined) ? '' : '' + str;
+    return str.replace(reg, function (match) {
+      return escapeMap[match];
+    });
+  };
+}(String));
+
 $(function()
 {
   $('pre code').each(function(i, block)
   {
     if(typeof $(this).attr('src') !== "undefined") {
-     $(this).load($(this).attr('src'));
+     $(this).load(escapeHtml($(this).attr('src')));
     }
   });
   hljs.initHighlightingOnLoad();  
