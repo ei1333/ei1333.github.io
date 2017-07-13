255e7e5ef69f435cd47053b944115a7bc4b77fab
ei1333 <missnakakun@gmail.com>
2017-07-13 16:58:50 +0900

    a

---
+++ algorithm/readsource2.js
@@ -0,0 +1,14 @@
+$(function()
+{
+  $('pre code').each(function(i, block)
+  {
+    if(typeof $(this).attr('src') !== "undefined") {
+      var poyo = $(this);
+      $.get($(this).attr('src'), function(data)
+      {
+        poyo.text(data);
+//        hljs.highlightBlock(block);
+      });
+    }
+  });
+});
