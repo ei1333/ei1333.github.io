bc52480381946d89383b58977427fb6f92499c90
ei1333 <missnakakun@gmail.com>
2017-11-22 09:21:41 +0900

    combination

--- algorithm/source/combination.cpp
+++ algorithm/source/combination.cpp
@@ -1,28 +1,56 @@
-typedef long long int64;
-const int mod = 1e9 + 7;
- 
-inline int64 modPow(int64 x, int64 n)
+struct Combination
 {
-  if(n == 0) return(1);
-  int64 ret = modPow(x, n / 2);
-  (ret *= ret) %= mod;
-  if(n & 1) (ret *= x) %= mod;
-  return(ret);
-}
-inline int64 modInv(int64 a)
-{
-  return(modPow(a, mod - 2));
-}
-inline int64 modCombination(int p, int q)
-{
-  static int64 fact[202020], rfact[202020];
-  if(fact[0] == 0) {
-    fact[0] = rfact[0] = 1;
-    for(int i = 1; i < 102020; i++) {
-      fact[i] = fact[i - 1] * i % mod;
-      rfact[i] = modInv(fact[i]);
+  int mod;
+  vector< int64_t > mfact, rfact;
+
+  Combination(int sz, int mod) : fact(sz + 1), rfact(sz + 1), mod(mod)
+  {
+    mfact[0] = 1;
+    for(int i = 1; i < mfact.size(); i++) {
+      mfact[i] = mfact[i - 1] * i % mod;
+    }
+    rfact[sz] = inv(mfact[sz]);
+    for(int i = sz - 1; i >= 0; i--) {
+      rfact[i] = rfact[i + 1] * (i + 1) % mod;
     }
   }
-  if(q < 0 || p < q) return(0);
-  return(fact[p] * rfact[q] % mod * rfact[p - q] % mod);
-}
+
+  int64_t fact(int k) const
+  {
+    return (mfact[k]);
+  }
+
+  int64_t pow(int64_t x, int64_t n) const
+  {
+    int64_t ret = 1;
+    while(n > 0) {
+      if(n & 1) (ret *= x) %= mod;
+      (x *= x) %= mod;
+      n >>= 1;
+    }
+    return (ret);
+  }
+
+  int64_t inv(int64_t x) const
+  {
+    return (pow(x, mod - 2));
+  }
+  
+  int64_t P(int n, int r) const
+  {
+    if(r < 0 || n < r) return (0);
+    return (mfact[n] * rfact[n - r] % mod);
+  }
+
+  int64_t C(int p, int q) const
+  {
+    if(q < 0 || p < q) return (0);
+    return (mfact[p] * rfact[q] % mod * rfact[p - q] % mod);
+  }
+
+  int64_t H(int n, int r) const
+  {
+    if(n < 0 || r < 0) return (0);
+    return (r == 0 ? 1 : C(n + r - 1, r));
+  }
+};
--- algorithm/source/get-combination.cpp
+++ algorithm/source/get-combination.cpp
@@ -3,9 +3,9 @@ vector< vector< int64_t > > get_combination(int N)
   vector< vector< int64_t > > mat(N + 1, vector< int64_t >(N + 1));
   for(int i = 0; i <= N; i++) {
     for(int j = 0; j <= i; j++) {
-      mat[i][j] = (j == 0 || j == i ? 1 : mat[i - 1][j - 1] + mat[i - 1][j]);
+      if(j == 0 || j == i) mat[i][j] = 1;
+      else mat[i][j] = mat[i - 1][j - 1] + mat[i - 1][j];
     }
   }
   return (mat);
 }
-
