c778f3a64ad0ad59aa49122ad219562ee4342ac3
ei1333 <missnakakun@gmail.com>
2018-02-28 16:34:16 +0900

    beet

--- algorithm/source/skew-heap.cpp
+++ algorithm/source/skew-heap.cpp
@@ -1,21 +1,31 @@
-template< typename T >
+template< typename T, typename E = T >
 struct SkewHeap
 {
+  using G = function< T(T, E) >;
+  using H = function< E(E, E) >;
+
   struct Node
   {
-    T key, lazy;
+    T key;
+    E lazy;
     Node *l, *r;
-  } *root = nullptr;
-  bool rev;
+  };
 
-  SkewHeap(bool rev = false) : rev(rev) {}
+  const bool rev;
+  const G g;
+  const H h;
+
+  SkewHeap(bool rev = false) : g([](const T &a, const E &b) { return a + b; }),
+                               h([](const E &a, const E &b) { return a + b; }), rev(rev) {}
+
+  SkewHeap(const G &g, const H &h, bool rev = false) : g(g), h(h), rev(rev) {}
 
   Node *propagate(Node *t)
   {
     if(t->lazy != 0) {
-      if(t->l) t->l->lazy += t->lazy;
-      if(t->r) t->r->lazy += t->lazy;
-      t->key += t->lazy;
+      if(t->l) t->l->lazy = h(t->l->lazy, t->lazy);
+      if(t->r) t->r->lazy = h(t->r->lazy, t->lazy);
+      t->key = g(t->key, t->lazy);
       t->lazy = 0;
     }
     return t;
@@ -31,18 +41,17 @@ struct SkewHeap
     return x;
   }
 
-  Node *push(const T &key)
+  void push(Node *&root, const T &key)
   {
-    auto t = new Node({key, 0, nullptr, nullptr});
-    root = merge(root, t);
+    root = merge(root, new Node({key, 0, nullptr, nullptr}));
   }
 
-  T top()
+  T top(Node *root)
   {
     return propagate(root)->key;
   }
 
-  void pop()
+  void pop(Node *&root)
   {
     propagate(root);
     auto *temp = root;
@@ -50,21 +59,21 @@ struct SkewHeap
     delete temp;
   }
 
-  bool empty() const
+  bool empty(Node *root) const
   {
     return !root;
   }
 
-  void add(const T &lazy)
+  void add(Node *root, const E &lazy)
   {
     if(root) {
-      root->lazy += lazy;
+      root->lazy = h(root->lazy, lazy);
       propagate(root);
     }
   }
 
-  void merge(SkewHeap< T > &p)
+  Node *makeheap()
   {
-    root = merge(root, p.root);
+    return nullptr;
   }
 };
