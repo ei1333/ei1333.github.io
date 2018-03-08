8d1470bdd8ffc468bb0741d58838183772af980f
ei1333 <missnakakun@gmail.com>
2018-03-08 21:53:50 +0900

    orega pro

--- algorithm/readsource.js
+++ algorithm/readsource.js
@@ -11,4 +11,7 @@ $(function()
       });
     }
   });
+
+document.queryCommandSupported("copy")&&($("pre").before('<div class="div-sample-copy"><span class="btn-sample-copy">Copy</span></div>'),$(".btn-sample-copy").click(function(){window.getSelection().removeAllRanges();try{var e=document.createRange();e.selectNode($(this).parent().next("pre").get()[0]),window.getSelection().addRange(e),document.execCommand("copy");var t=this;}catch(o){console.log(o)}window.getSelection().removeAllRanges()}));
+
 });
