4023ad422d2aa258032344746f5247e8b0775af7
ei1333 <missnakakun@gmail.com>
2018-03-12 17:47:20 +0900

    moebius

---
+++ algorithm/source/moebius.cpp
@@ -0,0 +1,24 @@
+map< int64_t, int > moebius(int64_t n) {
+  vector< int > factor;
+  for(int64_t i = 2; i * i <= n; i++) {
+    if(n % i == 0) {
+      factor.emplace_back(i);
+      while(n % i == 0) n /= i;
+    }
+  }
+  if(n > 1) factor.emplace_back(n);
+
+  map< int64_t, int > ret;
+  for(int i = 0; i < (1 << factor.size()); i++) {
+    int mu = 1;
+    int64_t dd = 1;
+    for(int j = 0; j < factor.size(); j++) {
+      if((i >> j) & 1) {
+        mu *= -1;
+        dd *= factor[j];
+      }
+    }
+    ret[dd] = mu;
+  }
+  return ret;
+}
