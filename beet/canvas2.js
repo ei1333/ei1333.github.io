8575613c51c61f8f93cea03e3089ff7c864e3031
ei1333 <missnakakun@gmail.com>
2018-03-16 01:43:20 +0900

    oi

--- beet/canvas2.js
+++ beet/canvas2.js
@@ -6,23 +6,42 @@ $(function()
   var base = new Image();
   base.src = "ushi.png";
   base.onload = function() {
-    ctx.drawImage(base, 0, 0, canvas.width(), canvas.height());
+    ctx.drawImage(base, 0, 0, 300, 200);
   };
 
-  $('#id').keyup(function(event)
+  $('#id1').keyup(function(event)
+  {
+    updateId();
+  });
+  $('#id2').keyup(function(event)
+  {
+    updateId();
+  });
+  $('#id3').keyup(function(event)
   {
     updateId();
   });
 
+
   function updateId()
   {
-    var str = $('#id').val();
+    var strText1 = $('#id1').val();
+    var str1 = Number(strText1) || 0;
+
+    var strText2 = $('#id2').val();
+    var str2 = Number(strText3) || 0;
+    str2 *= 4;
+
+    var strText3 = $('#id2').val();
+    var str3 = Number(strText3) || 0;
+    str3 *= 8;
+
     ctx.fillStyle="#FFFFFF";
-    ctx.fillRect(380, 140, 250, 170);
+    ctx.fillRect(195, 55, 77, 50);
     if(str.length != 0) {
       ctx.fillStyle="#000000";
-      ctx.font = "20px _sans";
-      ctx.fillText("("+str+")", 380, 300);
+      ctx.font = "10px _sans";
+      ctx.fillText("("+(str1+str2+str3)+" MB)", 195, 55);
     }
     var img_png_src = $("#row")[0].toDataURL("image/png");
     $("#output").attr("src", img_png_src);
